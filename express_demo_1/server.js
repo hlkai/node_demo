230842a628a45c2f989fe27fa6a2c461c30ca35c
xiaolie <hliekai@gmail.com>
2017-07-23 16:44:30 +0800

    express_demo_1  express中间件使用，日志morgan

--- express_demo_1/server.js
+++ express_demo_1/server.js
@@ -5,3 +5,12 @@ app.get("/", function(req, res) {
 });
 app.listen(1337);
 console.log("server running at http://localhost:1337/");
+
+// var favicon = require("serve-favicon");
+//app.use(express.favicon(__dirname, "/public/images/favicon.ico"));
+// app.use('/favicon.ico', express.static('images/favicon.ico'));
+
+var fs = require("fs");
+var logger = require("morgan");
+var accessLogStream = fs.createWriteStream(__dirname+'/access.log', {flags: 'a'});
+app.use(logger('combined', {stream: accessLogStream}));
